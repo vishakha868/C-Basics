1582920f33fbfaabcde4d1b8d6e3efd6c62c2774
vishakha868 <agrawalvishakha198@gmail.com>
2022-11-03 15:23:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? Ismajor = null;
+            if (Ismajor == true)
+            {
+                Console.WriteLine("user is major");
+            }
+            else if (Ismajor == false)
+            {
+                Console.WriteLine("user is not major");
+            }
+            else 
+            {
+                Console.WriteLine("user does not wish to say");
+            }
+        }
+    }
+}
